fb4788fc0250139073810507b20e875510bac1df
dianazhoha <dianazhoga@gmail.com>
2024-05-08 21:41:29 +0300

    Initial commit

---
+++ hw-1/hw.js
@@ -0,0 +1,41 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+//
+let q ='hello'
+let s = 'owu'
+let z = 'com'
+let d = 'ua'
+let f = 1
+let g = 10
+let h = -999
+let j = 123
+let k = 3.14
+let y = 2.7
+let t = true
+let ff = false
+console.log( q ,s, z , d)
+console.log(f, g,h,j)
+console.log( k, y,t, ff)
+
+
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+let firstName = 'Diana'
+let lastName = 'Zhoha'
+let middleName = 'Andriivna'
+let person = `${firstName} ${lastName} ${middleName}`
+console.log(person)
+
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a = 100
+let b = '100'
+let c = true
+
+console.log( a, b, c)
+console.log(typeof a, typeof b, typeof c)
+//
